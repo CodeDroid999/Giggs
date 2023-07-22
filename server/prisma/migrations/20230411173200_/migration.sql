b4b2fe4525e421a5085ff2baed9f2e9a0e90cfda
CodeDroid99 <johnmwega2002@gmail.com>
2023-07-22 16:58:50 +0300

    reastart everything

--- server/prisma/migrations/20230411173200_/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - The `isSocialLogin` column on the `User` table would be dropped and recreated. This will lead to data loss if there is data in the column.
-
-*/
--- AlterTable
-ALTER TABLE "User" DROP COLUMN "isSocialLogin",
-ADD COLUMN     "isSocialLogin" BOOLEAN;
--- server/routes/DashboardRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-
-import { verifyToken } from "../middlewares/AuthMiddleware.js";
-import { getSellerData } from "../controllers/DashboardController.js";
-
-export const dashboardRoutes = Router();
-
-dashboardRoutes.get("/seller", verifyToken, getSellerData);
